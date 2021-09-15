85b19f0ec8fc1ca454775ed2530d203dffbdb09d
Per Rutquist <per.rutquist@gmail.com>
2021-09-15 16:17:59 +0200

    refactor

--- src/NamedVecs.jl
+++ src/NamedVecs.jl
@@ -19,44 +19,72 @@ f(v) # returns [4, 5]
 ```
 
 """
-struct NamedVec{T,D<:AbstractVector{T},V<:NamedTuple} <: AbstractVector{T}
+struct NamedVec{T,Names,D<:AbstractVector{T},I,F} <: AbstractVector{T}
     data::D
-    views::V
-    function NamedVec(data::D, views::V) where {D<:AbstractVector{T}, V} where T
-        for v in views
-            v isa AbstractArray{T} || error("Wrong type view")
-        end
-        new{T,D,V}(data, views)
-    end
+    indices::NamedTuple{Names, I}
+    unvecs::NamedTuple{Names, F}
 end
 
 # Constructors
 
+function NamedVec(data::AbstractVector, names::NTuple{N, Symbol}, indices::Tuple{Vararg{Any,N}}, unvecs::Tuple{Vararg{Any,N}}) where {N}
+    NamedVec(data, NamedTuple{names}(indices), NamedTuple{names}(unvecs))
+end
+
 """
 A `NamedVec` can be created from a `NamedTuple` of arrays. The arrays are copied (not aliased) into the new object.
 """
-function NamedVec(xs::NamedTuple{Names, Tuple{Vararg{<:AbstractArray}}})
-    ns = map(length, xs)
+function NamedVec(xs::NamedTuple{Names, <:Tuple{AbstractArray, Vararg{AbstractArray}}}) where {Names}
+    ns = map(length, Tuple(xs))
     lastix = cumsum(ns)
-    firstix = lastix .- ns
+    firstix = lastix .- ns .+ 1
     data = similar(first(xs), (sum(ns),))
-    views = NamedTuple{names}(ntuple(i->viewlike(xs[i], data, firstix[i]:lastix[i]), length(xs)))
-    NamedVec(data, views)
+    v = NamedVec(data, Names, ntuple(i->firstix[i]:lastix[i], length(xs)), viewfun.(Tuple(xs)))
+    for n in Names
+        getproperty(v, n) .= getproperty(xs, n)
+    end
+    v
 end
 
-NamedVec(::NamedTuple{(), Tuple{}}) = error("A NamedVec must have at least one filed.")
+"""
+`viewfun(x)` - Returns a function that create an object that is like `x`, from a vector 
+with the correct number of elements.
+"""
+function viewfun(x::AbstractArray)
+    let sz = size(x)
+        v -> reshape(v, sz)
+    end
+end
+
+viewfun(x::AbstractVector) = identity
 
 # Conversion
 
-"`vec(v::NamedVec)` returns the underlying data `<:AbstractVector`."
+"`vec(v::NamedVec)` returns the underlying data vector of a NamedVec"
 Base.vec(v::NamedVec) = getfield(v, :data)
 
+"oftype(x::NamedVec, y) converts a vector y into a NamedVec with the names and indices of x"
+function Base.oftype(v::NamedVec{Names, <:Tuple{Vararg{AbstractArray}}}, w::AbstractVector) where {Names}
+    @boundscheck eachindex(v) == eachindex(w) || error("Size missmatch.")
+    NamedVec(w, indices(v), unvecs(v))
+end
+
+"""
+`Tuple(v::NamedVec)` returns a tuple of the views into `v`.
+For a tuple of the elements of the vector, use `Tuple(vec(v))`.
+"""
+function Base.Tuple(v::NamedVec) 
+    let data=vec(v)
+        map((i,f)->f(data[i]), Tuple(getfield(v, :indices)), Tuple(getfield(v, :unvecs)))
+    end
+end
+
 "`NamedTuple(v::NamedVec)` returns a `NamedTuple` of the views into `v`."
-Base.NamedTuple(v::NamedVec) = getfield(v, :views)
+Base.NamedTuple(v::NamedVec{<:Any,Names}) where {Names} = NamedTuple{Names}(Tuple(v))
 
 # Display
 
-function Base.show(io::IO, v::NamedVec)
+function Base.show(io::IO, ::MIME"text/plain", v::NamedVec)
     println(io, length(v), "-element NamedVec{", eltype(v), "}:")
     show(io, NamedTuple(v))
 end
@@ -64,31 +92,21 @@ end
 # Vector-like
 
 Base.length(v::NamedVec) = length(vec(v))
+Base.eachindex(v::NamedVec) = eachindex(vec(v))
 Base.eltype(v::NamedVec) = eltype(vec(v))
+Base.size(v::NamedVec) = size(vec(v))
+Base.size(v::NamedVec, dim::Integer) = size(vec(v), dim)
+
+#Base.:*(a::Number, v::NamedVec) = NamedVec(a * vec(v),
 
 # NamedTuple-like
 
-Base.getproperty(v::NamedVec, s::Symbol) = getproperty(NamedTuple(v), s)
-Base.getproperty(v::NamedVec, i::Integer) = getproperty(NamedTuple(v), i)
+Base.getproperty(v::NamedVec, s::Symbol) = view(vec(v), getproperty(indices(v), s)) |> getproperty(unvecs(v), s)
 
-"""
-`Tuple(v::NamedVec)` returns a tuple of the views into `v`.
-For a tuple of the elements of the vector, use `Tuple(vec(v))`.
-"""
-Base.Tuple(v::NamedVec) = Tuple(NamedTuple(v))
+# Utility functions
+# (These are unexported and mainly for internal use)
 
-end # module
+indices(v::NamedVec) = getfield(v, :indices)
+unvecs(v::NamedVec) = getfield(v, :unvecs)
 
-
-# # Utility functions
-
-# "`unanimous` is a utility function that checks that all elements of an iterable are identical, and returns one of them"
-# function unanimous(v)
-#     isempty(v) && throw("Vector must not be empty")
-#     v1 = first(v)
-#     length(v) == 1 && return v1
-#     for u in v
-#         u == v1 || error("Elements differ")
-#     end
-#     v1
-# end
+end # modulev
